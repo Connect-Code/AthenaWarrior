3fb0614247ad32b6df7e93b3ce2824b6375d5d94
MatthewVicente <41347019+MatthewVicente@users.noreply.github.com>
2021-02-04 15:22:41 -0300

    Change number on health bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,23 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
     public Gradient gradient;
     public Image fill;
+    public TextMeshProUGUI HP;
 
     public void SetMaxHealth(int health) {
         slider.maxValue = health;
         slider.value = health;
 
         fill.color = gradient.Evaluate(1f);
+
+        HP.text = string.Format("{0:n0}", health);
     }
 
     public void SetHealth(int health) {
         slider.value = health;
 
         fill.color = gradient.Evaluate(slider.normalizedValue);
+
+        HP.text = string.Format("{0:n0}", health);
     }
 }
