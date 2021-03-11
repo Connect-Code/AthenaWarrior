a8f755a4a46974d1afb97e0c0602b43ec15f4fee
MatthewVicente <41347019+MatthewVicente@users.noreply.github.com>
2021-03-11 14:44:47 -0300

    Add sfx and music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource deny_sfx, btn_click;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayClickBtn()
+    {
+        btn_click.Play();
+    }
+
+    public void PlayDenyBtn()
+    {
+        deny_sfx.Play();
+    }
+}
